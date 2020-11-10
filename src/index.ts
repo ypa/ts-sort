544329b752d85fa0acfe3b880d736d245599a983
Yan P Aung <yan.pye.aung@gmail.com>
2020-11-10 06:52:46 -0800

    Add boiler plate for Sorter class

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log("Hi there");
+class Sorter {
+
+  constructor(public collection: number[]) { }
+
+  sort(): void {
+
+  }
+
+}
+
+const sorter = new Sorter([10, 3, -5, 0]);
+sorter.sort();
+console.log(sorter.collection);

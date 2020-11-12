2d6e3405cd02004869c637677ad6cc59182033df
Yan P Aung <yan.pye.aung@gmail.com>
2020-11-12 07:03:49 -0800

    Test Sorter with CharactersCollection

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,15 @@
 import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
+import { CharactersCollection } from './CharactersCollection';
 
-const numbersCollection = new NumbersCollection([100000, 3, -5, 0]);
-const sorter = new Sorter(numbersCollection);
+// const numbersCollection = new NumbersCollection([50, -10, 0, -4]);
+// const sorter = new Sorter(numbersCollection);
+// sorter.sort();
+// console.log(numbersCollection.data);
+
+
+const charactersCollection = new CharactersCollection('Xatyb');
+const sorter = new Sorter(charactersCollection);
+
 sorter.sort();
-console.log(numbersCollection.data);
+console.log(charactersCollection.data);

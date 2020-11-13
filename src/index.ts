0ff44f42852e2169fad524b4b784d6699a1a4dd0
Yan P Aung <yan.pye.aung@gmail.com>
2020-11-13 07:18:22 -0800

    Test Sorter with LinkedList

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
 import { CharactersCollection } from './CharactersCollection';
+import { LinkedList } from './LinkedList';
 
 // const numbersCollection = new NumbersCollection([50, -10, 0, -4]);
 // const sorter = new Sorter(numbersCollection);
@@ -8,8 +9,17 @@ import { CharactersCollection } from './CharactersCollection';
 // console.log(numbersCollection.data);
 
 
-const charactersCollection = new CharactersCollection('Xatyb');
-const sorter = new Sorter(charactersCollection);
+// const charactersCollection = new CharactersCollection('Xatyb');
+// const sorter = new Sorter(charactersCollection);
+// sorter.sort();
+// console.log(charactersCollection.data);
 
+const linkedList = new LinkedList();
+linkedList.add(300);
+linkedList.add(-5);
+linkedList.add(-99);
+linkedList.add(11);
+
+const sorter = new Sorter(linkedList);
 sorter.sort();
-console.log(charactersCollection.data);
+linkedList.print();
